1450e738e5e0d0b07d6691853632801c2212a523
dianathreis <dtreis@hawaii.edu>
2024-11-07 21:44:41 -1000

    contact forms finished in 40 minutes

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -2,6 +2,7 @@
 
 import { Contact } from '@/lib/validationSchemas';
 import { Card, Image } from 'react-bootstrap';
+import Link from 'next/link';
 
 /* Renders a single row in the List Stuff table. See list/page.tsx. */
 const ContactCard = ({ contact }: { contact: Contact }) => (
@@ -18,6 +19,9 @@ const ContactCard = ({ contact }: { contact: Contact }) => (
     <Card.Body>
       <Card.Text>{contact.description}</Card.Text>
     </Card.Body>
+    <Card.Footer>
+      <Link href={`edit/${contact.id}`}>Edit</Link>
+    </Card.Footer>
   </Card>
 );
 
